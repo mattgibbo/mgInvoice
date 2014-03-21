f6fc5985aa72fcac6e39d1084dedf1c11198a211
mattgibbo <matt71020@gmail.com>
2014-03-21 09:59:53 +0000

    Initial styling and install form/info

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ mgInvoice
 =========
 
 Personal project to provide a PHP/MySQL powered invoicing system with the reports to breakdown earnings by months or year, according the the set tax year period.
+
+## Installing mgInvoice locally
+![mgInvoice install](/images/readme/install.jpg)
---
+++ install/install.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>mgInvoice Install</title>
+
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" id="extViewportMeta">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900|Open+Sans:300,400italic,400,600" rel="stylesheet" type="text/css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet" />
+    <link href="../css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+    <section class="pad-xl">
+        <h1 class="h-l clr-main--txt ls-1"><i aria-hidden="true" class="fa fa-coffee clr-alt--txt"></i> mgInvoice Install</h1>
+        <p class="ts-xl breadcrumb pad-l rounded">Installing mgInvoice is simple, just fill in the form below and you'll be good to go!</p>
+
+        <form>
+            <div class="frm-row mar-v-b">
+                <label class="frm-label va-m">Business Name:</label>
+                <input type="text" placeholder="Enter your business name here" class="frm-inp-txt va-m span-3" /><br /><br />
+                <span class="frm-helper frm-off ts-xs">This will be the default billing name, you can change this in the settings later.</span>
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label va-m">Your Name:</label>
+                <input type="text" placeholder="Enter your own name here" class="frm-inp-txt va-m span-3" />
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label va-m">Address:</label>
+                <input type="text" placeholder="" class="frm-inp-txt va-m span-3" />
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label"></label>
+                <input type="text" placeholder="" class="frm-inp-txt va-m span-3" />
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label">City</label>
+                <input type="text" placeholder="" class="frm-inp-txt va-m span-3" />
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label">County</label>
+                <input type="text" placeholder="" class="frm-inp-txt va-m span-3" />
+            </div>
+
+            <div class="frm-row">
+                <label class="frm-label va-m">Country</label>
+                <select class="frm-inp-txt va-m span-3">
+                    <option value="uk">United Kingdom</option>
+                </select>
+            </div>
+        </form>
+    </section>
+
+</body>
+</html>
